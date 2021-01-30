5d38dba4748ae00bf9cb9c1089f14253815af5bb
Praveen Gandla <Praveen.Gandla@jda.com>
2021-01-30 13:27:52 +0530

    Initial Checkin
    Initial Checkin

---
+++ GitPratices_ConsoleApp/GitPratices_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitPratices_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
